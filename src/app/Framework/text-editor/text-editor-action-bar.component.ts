92d9eace1f8dab2eb51a8617216b6ec2fa6e105c
Eric Wermelinger <eric.wermelinger@diartis.solutions>
2022-06-15 14:50:13 +0700

    Enhance Texteditor

--- src/app/Framework/text-editor/text-editor-action-bar.component.ts
+++ src/app/Framework/text-editor/text-editor-action-bar.component.ts
@@ -12,21 +12,29 @@ export class TextEditorActionBarComponent {
 
   @Input() editor!: IEditor;
   roosterJs = RoosterJs;
+  isTitle = false;
 
   editorFields: KeyValue<string, string>[] = [
+    { key: 'title', value: 'title'},
     { key: 'bold', value: 'format_bold'},
     { key: 'italic', value: 'format_italic'},
     { key: 'underline', value: 'format_underlined'},
     { key: 'strikethrough', value: 'format_strikethrough'},
     { key: 'bulletlist', value: 'format_list_bulleted'},
-    { key: 'numberlist', value: 'format_list_numbered'}
+    { key: 'numberlist', value: 'format_list_numbered'},
   ];
 
   constructor() { }
 
-  // todo color, highlight, font-size
   editorClick(eventKey: string, editor: IEditor) {
       switch (eventKey) {
+          case 'title':
+            const newFont = this.isTitle ? 1 : 0;
+            for (let i = 0; i < 3; i++) {
+              this.roosterJs.changeFontSize(editor, newFont);
+            }
+            this.isTitle = !this.isTitle;
+            break;
           case 'bold':
               this.roosterJs.toggleBold(editor);
               break;
