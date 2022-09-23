46d0e979d1c0187f5e1a38aa66fa0c645ff6dbd1
oliUmbi <oliver.umbricht@hispeed.ch>
2022-09-23 23:56:13 +0200

    rework

--- src/app/Pages/school-infos/school-infos.component.ts
+++ src/app/Pages/school-infos/school-infos.component.ts
@@ -11,39 +11,10 @@ import { SchoolInfosService } from './school-infos.service';
 export class SchoolInfosComponent {
 
   schoolInfos$: Observable<SchoolInfoDTO[]>;
-  columns = 3;
-  
+
   constructor(
     private schoolInfosService: SchoolInfosService,
   ) {
     this.schoolInfos$ = this.schoolInfosService.getSchoolInfos();
-    this.setColumns();
-  }
-
-  @HostListener("window:resize", [])
-  onResize() {
-    this.setColumns();
-  }
-
-  column1(infos: SchoolInfoDTO[]): SchoolInfoDTO[] {
-    return infos.filter((_, i) => i % this.columns === 0);
-  }
-
-  column2(infos: SchoolInfoDTO[]): SchoolInfoDTO[] {
-    return infos.filter((_, i) => i % this.columns === 1);
-  }
-
-  column3(infos: SchoolInfoDTO[]): SchoolInfoDTO[] {
-    return infos.filter((_, i) => i % this.columns === 2);
-  }
-
-  setColumns() {
-    if (window.innerWidth > 1360) {
-      this.columns = 3;
-    } else if (window.innerWidth > 700) {
-      this.columns = 2;
-    } else {
-      this.columns = 1;
-    }
   }
 }
